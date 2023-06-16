74076bc742226fd730b6d39217fbed92e376f2d9
Roberto <109102815+aaKIHITO@users.noreply.github.com>
2023-06-16 00:04:57 -0300

    v0.2

--- EndlessRunner/Assets/_Scripts/Ground & Scenario/Plot/PlotSpawner.cs
+++ EndlessRunner/Assets/_Scripts/Ground & Scenario/Plot/PlotSpawner.cs
@@ -13,21 +13,21 @@ public class PlotSpawner : MonoBehaviour
     [SerializeField] private GameObject _firstRightPlot; // Primeira casa da direita
     [SerializeField] private GameObject _parentObject; // Objeto-pai para as casas
 
+    private float _lastZPos; // Posição Z da última casa gerada
     private GameObject _lastLeftPlot; // Referência para a última casa da esquerda gerada
     private GameObject _lastRightPlot; // Referência para a última casa da direita gerada
 
     private void Start()
     {
-        // Instancia as primeiras casas da esquerda e direita
-        Instantiate(_firstLeftPlot, new Vector3(_LeftXPos, 0, 0), _firstLeftPlot.transform.rotation);
-        Instantiate(_firstRightPlot, new Vector3(_RightXPos, 0, 0), Quaternion.Euler(0, 180, 0));
+        // Posiciona a última Z com base na posição da primeira casa da esquerda
+        _lastZPos = _firstLeftPlot.transform.position.z;
 
-        _lastLeftPlot = _firstLeftPlot; // Define a primeira casa da esquerda como a última casa gerada
-        _lastRightPlot = _firstRightPlot; // Define a primeira casa da direita como a última casa gerada
+        // Define a primeira casa da direita como a última casa gerada
+        _lastRightPlot = _firstRightPlot;
 
         for (int i = 0; i < _initAmount; i++)
         {
-            SpawnPlot(); // Gera as casas adicionais
+            SpawnPlot(); // Gera as casas iniciais
         }
     }
 
@@ -45,7 +45,7 @@ public class PlotSpawner : MonoBehaviour
         while (leftPlot == _lastLeftPlot || leftPlot == _lastRightPlot || rightPlot == _lastLeftPlot || rightPlot == _lastRightPlot);
         // Repete o processo até que uma combinação não repetida seja encontrada
 
-        float zPos = _lastLeftPlot.transform.position.z + _plotSize; // Calcula a nova posição Z para a próxima casa
+        float zPos = _lastZPos + _plotSize; // Calcula a nova posição Z para a próxima casa
 
         // Instancia as casas da esquerda e direita como filhas do objeto-pai
         GameObject spawnedLeftPlot = Instantiate(leftPlot, new Vector3(_LeftXPos, 0, zPos), leftPlot.transform.rotation);
@@ -53,7 +53,8 @@ public class PlotSpawner : MonoBehaviour
         spawnedLeftPlot.transform.parent = _parentObject.transform;
         spawnedRightPlot.transform.parent = _parentObject.transform;
 
-        _lastLeftPlot = spawnedLeftPlot; // Atualiza a referência para a última casa da esquerda gerada
-        _lastRightPlot = spawnedRightPlot; // Atualiza a referência para a última casa da direita gerada
+        _lastZPos += _plotSize; // Atualiza a posição Z da última casa gerada
+        _lastLeftPlot = leftPlot; // Armazena a referência para a última casa da esquerda gerada
+        _lastRightPlot = rightPlot; // Armazena a referência para a última casa da direita gerada
     }
 }
